00c614ac00a46a15a174da893a1fd239cacee6df
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-09-26 14:59:45 -0700

    add auth to sidenav

--- client/src/components/Header/Sidenav/sidenav_items.js
+++ client/src/components/Header/Sidenav/sidenav_items.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import { connect } from 'react-redux';
 
-const SidenavItems = () => {
+const SidenavItems = ({ user }) => {
   const items = [
     {
       type: 'navItem',
@@ -16,14 +17,14 @@ const SidenavItems = () => {
       icon: 'file-text-o',
       text: 'My Profile',
       link: '/user',
-      restricted: false,
+      restricted: true,
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'Add Admins',
       link: '/user/register',
-      restricted: false,
+      restricted: true,
     },
     {
       type: 'navItem',
@@ -31,27 +32,28 @@ const SidenavItems = () => {
       text: 'Login',
       link: '/login',
       restricted: false,
+      exclude: true,
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'My Reviews',
       link: '/user/user-reviews',
-      restricted: false,
+      restricted: true,
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'Add Reviews',
       link: '/user/add',
-      restricted: false,
+      restricted: true,
     },
     {
       type: 'navItem',
       icon: 'fas fa-sign-out',
       text: 'Logout',
       link: '/user/logout',
-      restricted: false,
+      restricted: true,
     },
   ];
 
@@ -67,11 +69,22 @@ const SidenavItems = () => {
   };
 
   const showItems = () =>
-    items.map((item, i) => {
-      return element(item, i);
-    });
-
+    user.login
+      ? items.map((item, i) => {
+          if (user.login.isAuth) {
+            return !item.exclude ? element(item, i) : null;
+          } else {
+            return !item.restricted ? element(item, i) : null;
+          }
+        })
+      : null;
   return <div>{showItems()}</div>;
 };
 
-export default SidenavItems;
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+
+export default connect(mapStateToProps)(SidenavItems);
